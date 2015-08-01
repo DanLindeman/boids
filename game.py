b2539236ff6874e5710ca4a51a66b6748415dd4d
Dan Lindeman <lindemda@gmail.com>
2015-08-01 14:34:09 -0400

    Inital Drop

---
+++ game.py
@@ -0,0 +1,38 @@
+import pygame
+import boids
+import time
+from random import randint
+
+
+class Game(object):
+
+    def __init__(self):
+        pygame.display.set_caption('Boids')
+        width, height = (600, 600)
+        self.screen = pygame.display.set_mode((width, height))
+        self.env = boids.Environment(width, height)
+
+    def add_boids(self, number_of_boids):
+        for x in range(number_of_boids):
+            self.env.add_boid(self.env, x=randint(0, 600), y=randint(0, 600), speed=4, angle=randint(0, 360), awareness=300, separation=50)
+
+    def run_game(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif (event.type == pygame.MOUSEBUTTONDOWN) or (event.type == pygame.KEYDOWN):
+                    if (event.type == pygame.MOUSEBUTTONDOWN):
+                        print "MOUSE"
+            self.env.update()
+            self.screen.fill(self.env.colour)
+
+            for p in self.env.boids:
+                pygame.draw.circle(self.screen, p.colour, (int(p.x), int(p.y)), p.size, p.thickness)
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    game = Game()
+    game.add_boids(10)
+    game.run_game()
